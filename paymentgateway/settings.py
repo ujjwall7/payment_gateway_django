231fce9c17013b47a7153c11c707b4d40d52529e
ujjwall7 <sharmaujjwal0921@gmail.com>
2023-11-03 08:20:30 +0530

    add postgre

--- paymentgateway/settings.py
+++ paymentgateway/settings.py
@@ -76,11 +76,24 @@ WSGI_APPLICATION = 'paymentgateway.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'paymentgateway',
+         'USER': 'paymentgateway',
+         'PASSWORD': 'paymentgateway@321',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
 }
 
 
